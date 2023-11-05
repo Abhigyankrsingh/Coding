3eb4ef4433797f396c0e478b4d4b364f9617a364
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-05 16:06:30 +0530

    Corrected_it

--- count.cpp
+++ count.cpp
@@ -6,7 +6,7 @@ int count(int arr[], int n){
  
   int count = 0;
    for(int i =0; i<n; i++){
-    for(int j =i +1; i<n; j++){
+    for(int j =i +1; j<n; j++){
         if(arr[i] == arr[j]){
             count = count+1;
 
